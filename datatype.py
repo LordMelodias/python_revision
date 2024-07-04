2b61baaf658ebc68b03680dd50c5f56a02ab6437
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-07-04 08:48:51 +0000

    new file:   datatype.py

---
+++ datatype.py
@@ -0,0 +1,24 @@
+#  What is Data Type?
+# Data types is an attribute associated with a piece of data that tells a computer system how to interpret its value.
+
+# Different types of data ?
+"""
+1.	Numeric
+2.	Dictionary
+3.	Boolean
+4.	Set
+5.	Sequence type
+"""
+
+# Numeric
+# 1) Int, 2) Float, 3) Complex
+
+# Code:
+a = 5;
+print("the type of a ", type(a))
+
+b = 5.2
+print("the type of b ", type(b))
+
+c = 1+3j
+print("The type of c ", type(c))
