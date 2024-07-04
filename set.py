bba66e9963f2f628602cf9fd18ad2b5207ccac3c
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-07-04 11:24:46 +0000

    new file:   set.py 	new file:   tuple.py

---
+++ set.py
@@ -0,0 +1,18 @@
+set1 = {'surya', 'rohit', 'chawhan', 1000, 500}
+print(set1)
+
+#  add
+set1.add('Django')
+print(set1)
+
+# pop
+set1.pop()
+print(set1)
+
+# copy
+a = set1.copy()
+print(a)
+
+# set 
+set1.clear()
+print(set1)
---
+++ tuple.py
@@ -0,0 +1,11 @@
+tuple1 = ("Rohit", "Chawhan", "Python", "Django", 63.67)
+print(tuple1)
+print(type(tuple1))
+
+# count check how many time the elements is repeated
+a = tuple1.count("Rohit")
+print(a)
+
+# index check elements position in list in my chase rohit is o index position
+b = tuple1.index("Rohit")
+print(b)
