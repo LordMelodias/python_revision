6da880823275ebdee8388c736c0a0d08e686f9c1
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-07-04 10:20:07 +0000

    new file:   list.py

---
+++ list.py
@@ -0,0 +1,29 @@
+list1 = ['Hello', 'Rohit', 'Chawhan', 'Django']
+print(list1)
+print(list1[1])
+
+# append will add value from back
+list1.append('Flask')
+print(list1)
+
+# insert will add value by index position
+list1.insert(3, 'Python')
+print(list1)
+
+# pop will remove elements from back 
+list1.pop()
+print(list1)
+
+# reverse use to reverse elements of list
+list1.reverse()
+print(list1)
+
+# Copy used to copy the whole list with elements
+list2 = list1.copy()
+print(list2)
+
+# clear will remove all elements from list
+list1.clear()
+print(list1)
+
+
