de63595d97e5f6d240d6ba6b897794eeadcc8e39
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-07-06 13:27:37 +0000

    modified:   string_code.py

---
+++ string_code.py
@@ -0,0 +1,25 @@
+#  single line string
+str1 = "Rohit_Chawhan"
+print(type(str1))
+
+#  Multiple line string
+str2 = '''Hi I'm Rohit. I expert in Python. 
+I have knowledge about Django and Flask.
+'''
+print(str2)
+
+# String Indexing
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+# String split
+print(str1[0:])
+print(str1[6:])
+print(str1[:5])
+print(str1[0:5])
+
+# escape 
+print("I'm Rohit, \"I'm Python Developer.\"")  
