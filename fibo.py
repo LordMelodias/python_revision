ec8a86bc499e571904e84c6593aaec573e726d97
Rohit Chawhan <105154639+LordMelodias@users.noreply.github.com>
2024-08-16 09:01:00 +0530

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,8 +1,17 @@
 val = int(input("Enter a number: "))
 print("Entered number is ", val)
-fact =1
+n1 = 0
+n2 = 1
+count = 0
 
-for i in range(1,val+1):
-    fact= fact * i
-    
-print(fact, "!")
+if(val<=0):
+    print("Please enter a positive number")
+elif(val==n1):
+    print("Sequence: ",n1)
+else:
+    while(count<val):
+        print(n1)
+        nth = n1+n2
+        n1 = n2
+        n2 = nth
+        count+=1
